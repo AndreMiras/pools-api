a88ec46e1ede621c799d2bb87319c0a4937bb649
Andre Miras <andre.miras@gmail.com>
2020-10-16 23:11:45 +0200

    :wrench: Configurable allow origins headers
    ```
    heroku config:set ALLOW_ORIGINS='["http://localhost:3000"]'
    ```

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
 import experiment
 
 app = FastAPI()
+allow_origins = os.environ.get("ALLOW_ORIGINS", "[]")
+app.add_middleware(CORSMiddleware, allow_origins=allow_origins)
 
 
 @app.get("/")
