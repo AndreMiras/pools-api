1a7a05e14168e9d52bf3ab3f4c7bf9e81029a2b7
Andre Miras <andre.miras@gmail.com>
2020-10-18 16:27:23 +0200

    :white_check_mark: test_get_pair_info()

--- src/tests/test_libuniswaproi.py
+++ src/tests/test_libuniswaproi.py
@@ -3,6 +3,7 @@ from unittest import mock
 
 from .utils import (
     GQL_ETH_PRICE_RESPONSE,
+    GQL_PAIR_INFO_RESPONSE,
     patch_client_execute,
     patch_session_fetch_schema,
 )
@@ -21,3 +22,25 @@ class TestLibUniswapRoi:
         assert m_execute.call_count == 1
         assert eth_price == Decimal("321.123")
         assert str(eth_price) == GQL_ETH_PRICE_RESPONSE["bundle"]["ethPrice"]
+
+    def test_get_pair_info(self):
+        contract_address = "0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11"
+        m_execute = mock.Mock(return_value=GQL_PAIR_INFO_RESPONSE)
+        with patch_client_execute(m_execute), patch_session_fetch_schema():
+            pair_info = self.libuniswaproi.get_pair_info(contract_address)
+        assert m_execute.call_args_list == [
+            mock.call(
+                mock.ANY,
+                variable_values={"id": contract_address.lower()},
+            )
+        ]
+        assert pair_info["pair"].keys() == {
+            "id",
+            "reserve0",
+            "reserve1",
+            "token0",
+            "token0Price",
+            "token1",
+            "token1Price",
+            "totalSupply",
+        }
